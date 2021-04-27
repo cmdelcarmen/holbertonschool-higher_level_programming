ac6f1a611ec685ebbd81b7a698ddd534fc461ad5
cmdelcarmen <delcarmencaroline@gmail.com>
2021-04-27 19:41:39 +0000

    got rid of extra print

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 for count in range(100):
-    if count < 10:
-        print("0", end="")
     if count != 99:
-        print("{:d}".format(count), end=", ")
+        print("{0:02d}".format(count), end=", ")
     elif count == 99:
         print("{:d}".format(count))
