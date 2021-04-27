3f8bebc6e1005f4762d58ad3194b0fdc8800c52c
cmdelcarmen <delcarmencaroline@gmail.com>
2021-04-27 01:47:36 +0000

    update

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * check_cycle - function checks if linked list has cycles
+ * @list: head node
+ * Return: 0 if no, 1 if yes
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *current, *head;
+
+	current = list;
+	head = list;
+
+	if (list == NULL)
+		return (0);
+
+	while (current != NULL)
+	{
+		current = current->next;
+
+		if (current == head)
+		{
+			return (1);
+		}
+	}
+
+	return (0);
+}
