b0d3dc4f9d8061aed80f62a4610a5765664186b0
cmdelcarmen <delcarmencaroline@gmail.com>
2021-04-28 17:40:06 +0000

    fixed ans for num 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,10 +2,17 @@
 import random
 number = random.randint(-10000, 10000)
 
+isNegative = False
+
 if number < 0:
     number *= -1
+    isNegative = True
 
 lastDigit = number % 10
+
+if isNegative == True:
+    lastDigit *= -1
+    number *= -1
 
 print("Last digit of {:d} is {:d}".format(number, lastDigit), end=" ")
 
