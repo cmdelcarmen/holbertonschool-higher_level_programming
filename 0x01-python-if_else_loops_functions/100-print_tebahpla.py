d2c4ce6e9daaefafb83416e0c946260b0ce4c3b4
cmdelcarmen <delcarmencaroline@gmail.com>
2021-04-28 17:50:39 +0000

    fixed num 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,5 +2,7 @@
 
 
 for letter in range(90, 64, -1):
-
-        print("{}".format(chr(letter + 32)), end="" if letter % 2 == 0 else "{}".format(chr(letter)))
+    if letter % 2 == 0:
+        print("{}".format(chr(letter+32)), end="")
+    else:
+        print("{}".format(chr(letter)), end="")
