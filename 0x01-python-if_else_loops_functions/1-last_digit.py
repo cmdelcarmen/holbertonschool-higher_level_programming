d9525a73a081f9b01ce95fb0784737a4bae808f6
cmdelcarmen <delcarmencaroline@gmail.com>
2021-04-28 17:17:36 +0000

    changed conditional statment

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -13,5 +13,5 @@ if lastDigit > 5:
     print("and is greater than 5")
 elif lastDigit == 0:
     print("and is 0")
-elif lastDigit < 5:
+elif lastDigit <= 5:
     print("and is less than 6 and not 0")
