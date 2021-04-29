c3dbab722d8fbe20686ff00a89c88c237d7e9293
cmdelcarmen <delcarmencaroline@gmail.com>
2021-04-29 23:59:15 +0000

    fixing num 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,10 @@ import sys
 if __name__ == "__main__":
 
     if len(sys.argv) == 1:
-        print("{} argument".format(len(sys.argv)))
+        print("0 arguments.")
+
+    elif len(sys.argv) == 2:
+        print("{} argument".format(len(sys.argv) - 1))
 
     else:
-        print("{} arguments".format(len(sys.argv)))
+        print("{} arguments".format(len(sys.argv) - 1))
