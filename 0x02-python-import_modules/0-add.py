af99dbf855d24b44f7e1dcb910f4c877b7900351
cmdelcarmen <delcarmencaroline@gmail.com>
2021-04-29 21:39:52 +0000

    update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
+
 from add_0 import add
 
 if __name__ == "__main__":
 
     a = 1
     b = 2
-    c = add(1, 2)
-
-    print("{} + {} = {}".format(a, b, c))
-
+    
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
