19ee578b8ae44d567074a64be1efb64b35263e3e
cmdelcarmen <delcarmencaroline@gmail.com>
2021-04-29 22:34:43 +0000

    ans for num 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+import sys
+
+if __name__ == "__main__":
+
+    if len(sys.argv) == 1:
+        print("{} argument".format(len(sys.argv)))
+
+    else:
+        print("{} arguments".format(len(sys.argv)))
