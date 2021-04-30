64b14e087687e0582977f8efba0504d2506e9d23
cmdelcarmen <delcarmencaroline@gmail.com>
2021-04-30 02:56:23 +0000

    changed var names

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 from calculator_1 import add, sub, mul, div
 import sys
 
@@ -9,20 +10,20 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-    num1 = int(sys.argv[1])
-    num2 = int(sys.argv[3])
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
 
     if (sys.argv[2] == '+'):
-        print("{:d} + {:d} = {:d}".format(num1, num2, add(num1, num2)))
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 
     elif (sys.argv[2] == '-'):
-        print("{:d} - {:d} = {:d}".format(num1, num2, sub(num1, num2)))
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
 
     elif (sys.argv[2] == '*'):
-        print("{:d} * {:d} = {:d}".format(num1, num2, mul(num1, num2)))
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
 
     elif (sys.argv[2] == '/'):
-        print("{:d} / {:d} = {:d}".format(num1, num2, div(num1, num2)))
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
 
     else:
         print("Unknown operator. Available operators: +, -, * and /")
