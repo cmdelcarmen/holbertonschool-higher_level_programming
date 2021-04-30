35cf02e8197138a753de0c918b1981818f659a0a
cmdelcarmen <delcarmencaroline@gmail.com>
2021-04-30 01:13:38 +0000

    fixed for argv == 1

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,8 +4,10 @@ import sys
 
 if __name__ == "__main__":
 
-    if len(sys.argv) == 1:
-        print("0")
+    sumOfAllArgv = 0
 
-    else:
-        print(int(sys.argv[1]) + int(sys.argv[2]))
+    if len(sys.argv) > 1: 
+        for count in range(1, (len(sys.argv))):
+            sumOfAllArgv += int(sys.argv[count])
+
+        print(sumOfAllArgv)
