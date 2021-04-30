81f0bd932646999b7f894243f22460da607bf34e
cmdelcarmen <delcarmencaroline@gmail.com>
2021-04-30 00:12:34 +0000

    fixed num 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,6 +10,10 @@ if __name__ == "__main__":
 
     elif len(sys.argv) == 2:
         print("{} argument".format(len(sys.argv) - 1))
+        print("1: ", sys.argv[1])
 
     else:
         print("{} arguments".format(len(sys.argv) - 1))
+
+        for count in range(1, (len(sys.argv))):
+            print("{:d}:".format(count), sys.argv[count])
