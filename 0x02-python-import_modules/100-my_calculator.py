83caa74bc3e5ee7c9acd7c0cb3fb999ffd5b0cf8
cmdelcarmen <delcarmencaroline@gmail.com>
2021-04-30 07:33:48 +0000

    fixed import statement

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
 
-
 from calculator_1 import add, sub, mul, div
-import sys
+from sys import argv
 
 if __name__ == "__main__":
 
-    if len(sys.argv) != 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
+    a = int(argv[1])
+    b = int(argv[3])
 
-    if (sys.argv[2] == '+'):
+    if (argv[2] == '+'):
         print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 
-    elif (sys.argv[2] == '-'):
+    elif (argv[2] == '-'):
         print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
 
-    elif (sys.argv[2] == '*'):
+    elif (argv[2] == '*'):
         print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
 
-    elif (sys.argv[2] == '/'):
+    elif (argv[2] == '/'):
         print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
 
     else:
