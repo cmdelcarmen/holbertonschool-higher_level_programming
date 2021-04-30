606fc9e60d72746a5ce2ced8fc3f5e6b533772ec
cmdelcarmen <delcarmencaroline@gmail.com>
2021-04-30 01:07:42 +0000

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,4 +4,8 @@ import sys
 
 if __name__ == "__main__":
 
-    print(int(sys.argv[1]) + int(sys.argv[2]))
+    if len(sys.argv) == 1:
+        print("0")
+
+    else:
+        print(int(sys.argv[1]) + int(sys.argv[2]))
