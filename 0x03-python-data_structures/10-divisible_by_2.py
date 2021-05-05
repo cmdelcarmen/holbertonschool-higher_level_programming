b14dd1e888fae7fbf783e8452a8f2d9d6b1a985f
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-05 07:26:48 +0000

    ans for num 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def divisible_by_2(my_list=[]):
+
+    if len(my_list) == 0:
+        return None
+
+    new_list = []
+
+    for count in range(0, len(my_list)):
+        if my_list[count] % 2 == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
+
+    return new_list
