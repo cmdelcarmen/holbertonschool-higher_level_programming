2153e535dfbb96af7c6596566b64061c8e691157
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-05 06:21:05 +0000

    fix spaces num 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,5 +5,8 @@ def print_matrix_integer(matrix=[[]]):
 
     for count in range(len(matrix)):
         for count1 in range(len(matrix[count])):
-            print("{:d} ".format(matrix[count][count1]), end="")
+            print("{:d}".format(matrix[count][count1]), end="")
+
+            if count1 != ((len(matrix) - 1)):
+                print(" ", end ="")
         print()
