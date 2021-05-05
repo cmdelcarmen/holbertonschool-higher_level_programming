23ff8d1a669f39eb46d04a2cb0db1ed714d2076a
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-05 14:24:09 +0000

    fixed condition

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -10,6 +10,6 @@ def print_matrix_integer(matrix=[[]]):
         for count1 in range(len(matrix[count])):
             print("{:d}".format(matrix[count][count1]), end="")
 
-            if count1 != ((len(matrix) - 1)):
+            if count1 < ((len(matrix) - 1)):
                 print(" ", end="")
         print()
