4e808433df17f0edf1690e233e2ad985418cdb19
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-05 03:55:39 +0000

    fixed req fail for num 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,11 @@
 
 def no_c(my_string):
 
-    new_string = my_string.replace('c', '')
-    new_string = new_string.replace('C', '')
+    new_string = ""
+
+    for count in range(0, len(my_string)):
+        if ord(my_string[count]) != ord("c"):
+            if ord(my_string[count]) != ord("C"):
+                    new_string = new_string + my_string[count]
+
     return new_string
