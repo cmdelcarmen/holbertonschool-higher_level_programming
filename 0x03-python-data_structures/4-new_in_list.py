95abf7a2c703d8c73f94cd222d25af37ad1a6ee7
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-05 06:08:45 +0000

    ans num 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -8,7 +8,7 @@ def new_in_list(my_list, idx, element):
     if idx < 0:
         return newCopy_my_list
 
-    if idx > len(my_list):
+    if idx > (len(my_list) - 1):
         return newCopy_my_list
 
     newCopy_my_list[idx] = element
