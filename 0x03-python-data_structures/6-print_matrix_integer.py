61c0039bcd011ed7c8302edf8450652a1f9490f6
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-05 04:54:11 +0000

    ans for num 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+
+    for count in range(0, 3):
+        for count1 in range(0 , 3):
+            print(matrix[count][count1], end=" ")
+        print()
