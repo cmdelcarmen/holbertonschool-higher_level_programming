e8a7f34b4c49d7551b8c55305e224486f227b144
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-06 17:42:30 +0000

    ans for num 2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def uniq_add(my_list=[]):
+
+    total = 0
+    numbersAdded = []
+
+    if len(my_list) == 0:
+        return None
+
+    total += my_list[0]
+    numbersAdded.append(my_list[0])
+
+    for number in my_list:
+        if number not in numbersAdded:
+            total += number
+            numbersAdded.append(number)
+
+    return total
