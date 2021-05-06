45c363e5a855120d90b6f1d8848a24646b66291b
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-06 03:23:13 +0000

    empty sentence case fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,11 +4,11 @@
 def multiple_returns(sentence):
 
     if len(sentence) == 0:
-        sentence += 'None'
+        sentence_tuple = 0, None
+        return sentence_tuple
 
     lengthOfSentence = len(sentence)
     firstLetterOfSentence = sentence[0]
-
     sentence_tuple = lengthOfSentence, firstLetterOfSentence
 
     return sentence_tuple
