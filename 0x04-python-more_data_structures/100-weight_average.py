4951a26a57b229dff5cad18a358fc23e123f632f
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-06 21:52:44 +0000

    update

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -6,6 +6,9 @@ def weight_average(my_list=[]):
     average = 0
     divisor = 0
 
+    if len(my_list) == 0:
+            return 0
+
     for score in my_list:
         average += (score[0] * score[1])
         divisor += score[1]
