8755cc2bdb7ff705b35883e93e509b40b8da21f8
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-06 17:23:55 +0000

    update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,27 +3,13 @@
 
 def square_matrix_simple(matrix=[]):
 
+    newList = []
     newMatrix = []
-    secondMatrix = []
-    thirdMatrix = []
 
-    if len(matrix) == 0:
-        return None
-
-    for x in range(1):
+    for x in range(len(matrix)):
         for y in range(len(matrix)):
-            squaredMatrixValue = ((matrix[x][y]) ** 2)
-            newMatrix.append(squaredMatrixValue)
+            newList.append(matrix[x][y] ** 2)
+        newMatrix += [newList]
+        newList = []
 
-    for x in range(1, 2):
-        for y in range(len(matrix)):
-            squaredMatrixValue = ((matrix[x][y]) ** 2)
-            secondMatrix.append(squaredMatrixValue)
-
-    for x in range(2, 3):
-        for y in range(len(matrix)):
-            squaredMatrixValue = ((matrix[x][y]) ** 2)
-            thirdMatrix.append(squaredMatrixValue)
-
-    newMatrix = [newMatrix, secondMatrix, thirdMatrix]
-    return newMatrix
+    return(newMatrix)
