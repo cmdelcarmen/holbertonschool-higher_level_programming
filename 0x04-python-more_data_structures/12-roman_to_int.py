37103c851c40c3db09e92c72897741b673cf4459
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-06 21:27:43 +0000

    ans 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,13 +6,14 @@ def roman_to_int(roman_string):
     romanDi = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     integer = 0
     prevValue = 0
-    new_roman_string = ''.join(reversed(roman_string))
 
-    if len(new_roman_string) == 0:
+    if roman_string is None:
         return 0
 
     if not isinstance(roman_string, str):
         return 0
+
+    new_roman_string = ''.join(reversed(roman_string))
 
     for romanLetter in new_roman_string:
         if romanLetter not in romanDi:
