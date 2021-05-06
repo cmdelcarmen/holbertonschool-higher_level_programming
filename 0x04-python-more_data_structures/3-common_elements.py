d7c43e89a475b87bc7c5668489a9e13b421c0be4
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-06 17:54:48 +0000

    ans for num 3

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def common_elements(set_1, set_2):
+
+    list1 = set(set_1)
+    list2 = set(set_2)
+    newSet = {""}
+    index = 0
+
+    for word in list1:
+        if word in list2:
+            newSet.add(word)
+
+    newSet.remove("")
+    return newSet
