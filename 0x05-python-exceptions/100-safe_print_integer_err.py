e01133711a62a5c467817e2d9d2cb0de28b1a178
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-17 18:47:14 +0000

    ans for num 7

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer_err(value):
+
+    try:
+        print("{:d}".format(value))
+        return True
+
+    except ValueError:
+        print("Exeception: Unknown format code 'd' for object of type 'str'")
+        return False
