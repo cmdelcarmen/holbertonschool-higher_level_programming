fd1b2920b879cdfaa05adc77f23d879b352f154f
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-17 17:30:09 +0000

    ans for num 2/4

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for element in range(0, x):
         try:
             print("{:d}".format(my_list[element]), end="")
-            int_totalPrintedNums = int_totalPrintedNums + 1
+            int_totalPrintedNums += 1
         except:
             continue
 
