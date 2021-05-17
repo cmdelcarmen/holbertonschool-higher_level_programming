f817fc3bb7dac2ce398f6059737f9056eeed376a
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-17 17:45:00 +0000

    revert ans num 2 to old ans

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,7 +9,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[element]), end="")
             int_totalPrintedNums += 1
-        except(TypeError, IndexError):
+        except:
             continue
 
     print()
