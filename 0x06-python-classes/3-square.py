fcf2bc39c9cb55bd20691886617df1f8bf3a1fe0
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-18 19:39:51 +0000

    comments

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+'''This module defines an object class called Squares'''
 
 
 class Square:
-
+    '''This class contains a private attribute called class'''
     def __init__(self, size=0):
+        '''Initializes private class attribute size'''
+
         if not type(size) is int:
             raise TypeError("size must be an integer")
         if size < 0:
@@ -11,4 +14,5 @@ class Square:
         self.__size = size
 
     def area(self):
+        '''Calculates area is Square instance'''
         return (self.__size * self.__size)
