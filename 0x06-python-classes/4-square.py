60d02ba6d704f99b1f3c7cbf4b7e76826307ce2c
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-18 19:46:52 +0000

    comment

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
+'''This module defines an object class called Squares'''
 
 
 class Square:
-
+    '''This class contains a private attribute called class'''
     def __init__(self, size=0):
+        '''Initializes private class attribute size'''
         self.__size = size
 
     @property
     def size(self):
+        '''Class method returns value of instance'''
         return self.__size
 
     @size.setter
     def size(self, value):
+        '''Class method returns value of instance'''
         if not type(value) is int:
             raise TypeError("size must be integer")
         if value < 0:
@@ -19,4 +23,5 @@ class Square:
         self.__size = value
 
     def area(self):
+        '''Class method returns area'''
         return (self.__size * self.__size)
