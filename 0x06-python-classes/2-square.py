6de93a0f775181f0bfde9a50615dc3b3b75ca020
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-19 04:46:13 +0000

    fixed spelling

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,5 +9,5 @@ class Square:
         if not type(size) is int:
             raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError("size must be >=0")
+            raise ValueError("size must be >= 0")
         self.__size = size
