1788df8f33ad08ef92ab616ad023d80212051864
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-19 02:57:38 +0000

    comments

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,8 +5,9 @@
 class Square:
     '''This class contains a size attribute'''
     def __init__(self, size=0):
+        ''' This metho init the class'''
         if not type(size) is int:
             raise TypeError("size must be an integer")
-        if (size < 0):
+        if size < 0:
             raise ValueError("size must be >=0")
         self.__size = size
