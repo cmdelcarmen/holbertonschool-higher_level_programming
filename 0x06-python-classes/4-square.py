f700bcf8aa849cdb613b83df94ffd373729d220b
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-19 04:44:21 +0000

    update spelling

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -17,7 +17,7 @@ class Square:
     def size(self, value):
         '''Class method returns value of instance'''
         if not type(value) is int:
-            raise TypeError("size must be integer")
+            raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
