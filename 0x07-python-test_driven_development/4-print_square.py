43c09cddadb7122834930f1207a5cacc8214979b
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-21 03:33:41 +0000

    update

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''prints a square'''
+
+def print_square(size):
+    '''prints a square'''
+
+    if not type(size) is int:
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for count in range(0,size):
+        for count2 in range(0,size):
+            print("#", end="")
+        print()
