c694f6aff3f0f5c4df75bc6f714146c8fb0f0f0a
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-23 00:33:39 +0000

    updated while loop

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -8,20 +8,28 @@ def text_indentation(text):
     Printed lines cannot start or end with spaces'''
 
     int_index = 0
-    beginning_spaces = 1
+    int_beginning_spaces = 1
 
     while (int_index < len(text)):
-        if beginning_spaces == 1:
+        if int_beginning_spaces == 1:
             '''takes care of input that start with a bunch of spaces'''
             while(text[int_index] == " "):
                 int_index += 1
-            beginning_spaces = 0
+            int_beginning_spaces = 0
 
         print(text[int_index], end="")
 
         if text[int_index] == '.' or text[int_index] == ':' or text[int_index] == '?':
                  print()
                  print()
-                 int_index+= 1
+                 if int_index < (len(text) - 1):
+                     '''makes sure program is not trying to read an out of range value'''
+                     if text[int_index + 1] == " ":
+                        '''makes sure next printed line does not start with a space'''
+                        while(text[int_index + 1] == " " and (int_index < (len(text) - 1))):
+                            int_index += 1
+                            if int_index >= len(text) - 1:
+                                '''breaks before trying to read an out of range value'''
+                                break
 
         int_index += 1
