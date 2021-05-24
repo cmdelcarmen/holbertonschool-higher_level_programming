92db04bbd49e5872147d56b0bbad78285e4c104a
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-24 23:24:41 +0000

    fixed error message

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -34,7 +34,7 @@ class Rectangle:
     def height(self, value):
         '''validates height input and defines it'''
         if type(value) is not int:
-            raise TypeError("heigth must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("heigth must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
