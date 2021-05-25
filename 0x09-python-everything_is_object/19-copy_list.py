c97401bdcbd329398659f5f6badbdfcc180d3c51
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-25 23:20:56 +0000

    ans for 19

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-'''Defines copy_list function'''
-
-
 def copy_list(l):
-    '''Function returns a copy of a list'''
-
-    new_list = l.copy()
-    return (new_list)
+    return (l.copy())
