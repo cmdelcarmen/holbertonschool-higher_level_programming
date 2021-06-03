d39630b691a3ef052d0c2f181afd8260367784f4
cmdelcarmen <delcarmencaroline@gmail.com>
2021-06-03 14:49:33 +0000

    ans for num 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''comments'''
+
+
+class BaseGeometry:
+    '''comments'''
+
+    def __init__(self, width, height):
+        '''comments'''
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        '''comments'''
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        '''comments'''
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    '''comments'''
+
+    def __init__(self, width, height):
+        '''comments'''
+        BaseGeometry.integer_validator(self, "string", height)
+        BaseGeometry.integer_validator(self, "string", height)
+
+        self.__width = width
+        self.__height = height
