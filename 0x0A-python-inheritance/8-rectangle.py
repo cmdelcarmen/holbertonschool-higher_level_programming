bb763d1cea0811fb37bc54f30c7dcba1b75f7681
cmdelcarmen <delcarmencaroline@gmail.com>
2021-06-05 22:52:08 +0000

    fixed import issue

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,24 +2,7 @@
 '''comments'''
 
 
-class BaseGeometry:
-    '''comments'''
-
-    def __init__(self, width, height):
-        '''comments'''
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        '''comments'''
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        '''comments'''
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
