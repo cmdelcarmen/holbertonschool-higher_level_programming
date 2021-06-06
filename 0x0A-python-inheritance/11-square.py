74e99a88601377866b44d235335c45b7eb32dcb1
cmdelcarmen <delcarmencaroline@gmail.com>
2021-06-06 01:50:07 +0000

    ans for num 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -11,3 +11,8 @@ class Square(Rectangle):
         '''comments'''
         self.integer_validator("size", size)
         super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        '''comments'''
+        return ("[Square] {}/{}".format(self.__size, self.__size))
