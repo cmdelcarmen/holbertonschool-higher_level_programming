3a619bf86fbc82f08a2d9bcabd85f2daebdb32a1
cmdelcarmen <delcarmencaroline@gmail.com>
2021-06-07 03:56:48 +0000

    ans for num 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''comments'''
+
+
+class Student:
+    '''comments'''
+
+    def __init__(self, first_name, last_name, age):
+        '''comments'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''comments'''
+
+        attrs_dict = {}
+
+        if type(attrs) is list:
+            if all(type(element) is str for element in attrs):
+                for key in self.__dict__:
+                    if key in attrs:
+                        attrs_dict[key] = self.__dict__[key]
+                return attrs_dict
+
+        return self.__dict__
