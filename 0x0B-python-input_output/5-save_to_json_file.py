008cad80842092c3734ed2ed7e017b7766f615b4
cmdelcarmen <delcarmencaroline@gmail.com>
2021-06-07 00:52:20 +0000

    update

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,7 +8,5 @@ import json
 def save_to_json_file(my_obj, filename):
     '''comments'''
 
-    new_content = json.dumps(my_obj)
-
-    with open(filename, 'w+') as txt_file:
-        txt_file.write(new_content)
+    with open(filename, 'w+') as filename:
+        json.dump(my_obj, filename)
