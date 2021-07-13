7da661e9893dc6059aac59bc5330648a0b7e69cc
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-13 17:03:52 +0000

    pucshing answer to number 4

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- Written by Caroline Del Carmen for Holberton Programming School
+-- Script creates a table called 'first_table' in the current database in the MySQL server.
+CREATE table first_table (id INT,
+name VARCHAR(256))
