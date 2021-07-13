fa828625bb9986ac30661a154a54d0b646cbdb1e
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-13 16:34:06 +0000

    pushing 1 & 2

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Written by Caroline Del Carmen for Holberton Programming School.
+-- Script create database 'hbtn_0c_0' in my MySQL server.
+CREATE DATABASE hbtn_0c_0
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- Written by Caroline Del Carmen for Holberton Programming School.
+-- Script deletes database 'hbtn_0c_0' in my MySQL server.
+DROP DATABASE IF EXISTS hbtn_0c_0
