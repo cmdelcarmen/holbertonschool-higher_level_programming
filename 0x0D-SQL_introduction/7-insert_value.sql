50f66df306fd8b73ab1575ecaf6ee8ef1afb426a
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-13 19:15:05 +0000

    pushing answer for 7

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- Written by Caroline Del Carmen for Holberton Programming School
+-- Script inserts a new row in the table first_table (database hbtn_0c_0)
+-- in the MySQL server
+INSERT INTO first_table (id, name) VALUES(89, "Holberton School");
