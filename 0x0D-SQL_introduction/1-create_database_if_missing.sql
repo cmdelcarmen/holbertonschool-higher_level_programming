b9c6dbfb804893f5ebfeab193fe5df07a48f6306
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-13 16:35:39 +0000

    added if statement

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- Written by Caroline Del Carmen for Holberton Programming School.
 -- Script create database 'hbtn_0c_0' in my MySQL server.
-CREATE DATABASE hbtn_0c_0
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
