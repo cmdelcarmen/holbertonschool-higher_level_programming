51d19a72cd210ec5ce6bd7b999ccf0695c432e1c
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-13 17:22:24 +0000

    pushing ans for 5

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- Written by Caroline Del Carmen for Holberton Programming School
+-- Script prints the full description of the table 'first_table' from
+-- the database 'hbtn_0c_0' in the MySQL server.
+DESCRIBE first_table;
