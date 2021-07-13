3ffc74c49b6fc76666ec9d4793faa246677317f0
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-13 17:28:11 +0000

    updated ans

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- Written by Caroline Del Carmen for Holberton Programming School
 -- Script prints the full description of the table 'first_table' from
 -- the database 'hbtn_0c_0' in the MySQL server.
-SHOW first_table;
+SHOW CREATE TABLE first_table;
