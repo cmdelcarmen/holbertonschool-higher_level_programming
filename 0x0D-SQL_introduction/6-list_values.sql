eec1f0f73ef48c073ade6f0267aacf361e6fa696
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-13 18:20:23 +0000

    pushign ans for 6'

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,4 @@
+-- Written by Caroline Del Carmen for Holberton Programming School
+-- Script lists all rows of the table first_table from the
+-- database 'hbtn_0c_0' in your MySQL server
+SELECT * FROM first_table
