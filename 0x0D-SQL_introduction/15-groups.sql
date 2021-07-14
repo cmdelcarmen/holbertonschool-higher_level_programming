b6262245533b4a95d948092f868f104051c6f148
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-14 02:53:41 +0000

    update

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- Written by Caroline Del Carmen for Holberton Programming School
 -- Script lists the number of records with the same score in the table
 -- 'second_table' of the database hbtn_0c_0 in MySQL srever
-SELECT score, COUNT(*) AS numer FROM second_table
-GROUP BY sore ORDER BY number DESC;
+SELECT score, COUNT(*) AS number FROM second_table
+GROUP BY score ORDER BY number DESC;
