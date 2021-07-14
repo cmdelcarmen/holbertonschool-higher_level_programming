5d6deae8f381049aaba0d8d3e492831888f9e17e
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-14 02:21:08 +0000

    update

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,3 @@
+-- Written by Caroline Del Carmen for Holberton Programming School
+-- Script updates the score of Bob to 10 in the 'second_table'
+UPDATE second_table SET score = 10 WHERE name = "Bob";
