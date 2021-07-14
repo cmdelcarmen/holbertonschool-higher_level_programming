86a6054c9ca1dc7cb492e4b254aa672fc5e61599
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-14 02:07:13 +0000

    fixed mysql keyword

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
 -- Written by Caroline Del Carmen for Holberton Programming School
 -- Script lists all records of the table 'second_table' of the database
--- 'hbtn_0c_0 in the MySQL server'
-SELECT score, name from second_table
+-- 'hbtn_0c_0' in the MySQL server
+SELECT score, name FROM second_table
 ORDER BY score DESC;
