d688b04c13e4107d4fb7a99b29baa21271ac85a5
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-14 02:43:41 +0000

    pushing ans to number 14

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- Written by Caroline Del Carmen for Holberton Programming School
+-- Script computes the score average of all records in the table
+-- 'second_table of the database 'hbtn_0c_0' in the MySQL server
+SELECT AVG(score) AS average FROM second_table;
