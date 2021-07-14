50e6076160b7befcefd815397d75b2bd2b33caa7
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-14 02:25:28 +0000

    pushing answer for number 12

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,4 @@
+-- Written by Caroline Del Carmen for Holberton Programming School
+-- Script removes all records with a score <=5 in the 'second_table'
+-- of the database 'hbtn_0c_0 in the mysql server'
+DELETE FROM second_table WHERE score <= 5;
