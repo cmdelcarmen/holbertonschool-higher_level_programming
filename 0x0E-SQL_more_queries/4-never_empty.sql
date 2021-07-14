7a205d2b971e1b675decdd5bcf063c1dfadb4c07
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-14 19:05:17 +0000

    pushing answer for 4

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,5 @@
+-- Written by Caroline Del Carmen for Holberton Programming School
+-- Script creates the table id_not_null on MySQL server
+CREATE TABLE IF NOT EXISTS id_not_null(
+id INT DEFAULT 1,
+name VARCHAR(256));
