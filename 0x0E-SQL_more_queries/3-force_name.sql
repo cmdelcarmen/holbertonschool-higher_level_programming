73d7033ec875a16a96fdea09535ae5d72b6ca164
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-14 18:55:46 +0000

    pushing answers to 0, 1, 2,3

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,5 @@
+-- Written by Caroline Del Carmen for Holberton Programming School
+-- Script creates the table force_name on the MySQL server.
+CREATE TABLE IF NOT EXISTS force_name
+(id INT,
+name VARCHAR(256) NOT NULL)
