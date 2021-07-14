e082da3025d9ec6e2edfe03950b07e902d22529b
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-14 19:07:49 +0000

    pushing answer for 5

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,5 @@
+-- Written by Caroline Del Carmen for Holberton Programming School
+-- Script creates the table unique_id on the MySQL server
+CREATE TABLE IF NOT EXISTS unique_id(
+id INT DEFAULT 1 UNIQUE,
+name VARCHAR(256));
