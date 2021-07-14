103b3a8ae28c9dd402e68f67d217258d773d4fbd
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-14 01:58:11 +0000

    pushing ans for num 10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Written by Caroline Del Carmen for Holberton Programming School
+-- Script lists all records of the table 'second_table' of the database
+-- 'hbtn_0c_0 in the MySQL server'
+SELECT score, name from second_table
+ORDER BY score DESC;
