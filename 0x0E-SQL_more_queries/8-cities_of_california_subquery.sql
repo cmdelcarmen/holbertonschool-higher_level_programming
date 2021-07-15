ea2221a9bf28ff8c14dbe334be32b041e3666d42
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-15 03:09:50 +0000

    pushing nas for 8

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- Written by Caroline Del Carmen for Holberton Programming School
+-- Script lists all the cities of California that can be found
+-- in the database hbtn_0d_usa
+SELECT id, name FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY id ASC;
