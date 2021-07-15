d8cad3adc57c274ceda512bbeb497cb6377fce80
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-15 03:13:11 +0000

    pushing ans for num 10

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- Written by Caroline Del Carmen for Holberton Programming School
+-- Scriptcript that lists all shows contained in hbtn_0d_tvshows
+-- that have at least one genre linked.
+SELECT id, name FROM cities WHERE state_id IN
+(SELECT id FROM states WHERE name = "California")
+ORDER BY id ASC;
