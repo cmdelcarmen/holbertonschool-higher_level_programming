6099e26e50377a66debcb6446a3b94d5071235f1
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-15 02:58:11 +0000

    fixed typo

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- Written by Caroline Del Carmen for Holberton Programming School
 -- Script creates the database hbtn_0d_usa and the table states
 -- (in the database hbtn_0d_usa) on the MySQL server
-REATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+cREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
 id INT NOT NULL AUTO_INCREMENT,
 name VARCHAR(256) NOT NULL);
