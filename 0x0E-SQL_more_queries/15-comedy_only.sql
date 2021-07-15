2418f80d400b49c35cab6bb1b10766f5a73056dc
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-15 17:32:36 +0000

    pushign ans for num 15

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- Written by Caroline Del Carmen for Holberton Programming School
+-- Script lists all genres from hbtn_0d_tvshows and displays the
+-- number of shows linked to each
+SELECT tv_shows.title FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy' ORDER BY tv_shows.title ASC;
