64660a2fc194cd60d93b7c10af698c6ffce6862c
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-15 03:15:43 +0000

    fixed ans for num 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,9 @@
 -- Written by Caroline Del Carmen for Holberton Programming School
 -- Scriptcript that lists all shows contained in hbtn_0d_tvshows
 -- that have at least one genre linked.
-SELECT id, name FROM cities
-WHERE state_id IN
-(SELECT id FROM states WHERE name = "California")
-ORDER BY id ASC;
+SELECT T_S.title, T_S_G.genre_id
+FROM tv_show_genres AS T_S_G
+INNER JOIN tv_shows AS T_S
+ON T_S_G.show_id = T_S.id
+ORDER BY T_S.title ASC,
+T_S_G.genre_id;
