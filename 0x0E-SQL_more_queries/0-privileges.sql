58dd9d85503d6ba0e1564519513d7a5d367ae57d
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-15 17:37:12 +0000

    update

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- Written by Caroline Del Carmen for Holberton Programming School
--- Script lists all priveleges of the MySQL users: user_0d_1 and
--- user_0d_2 on the server.
+
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
