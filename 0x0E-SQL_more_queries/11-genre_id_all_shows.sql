0ca3389c611b1cf70d397b3259154a40d279ad0d
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-15 03:23:09 +0000

    pushing ans for 11

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- Written by Caroline Del Carmen for Holberton Programming School
+-- Script lists all shows contained in the database hbtn_0d_tvshows
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC
