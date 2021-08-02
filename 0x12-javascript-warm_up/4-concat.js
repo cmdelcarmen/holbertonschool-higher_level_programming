e83e98990b1354638d52a00f3661b66c9ddbba92
cmdelcarmen <delcarmencaroline@gmail.com>
2021-08-02 23:20:15 +0000

    Script prints two arguments passed to it

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+/*
+ * Script prints two arguments passed to it,
+ * in the following format: "is".
+ */
+
+const args = process.argv;
+const str = args[2] + ' is ' + args[3];
+
+console.log(str);
