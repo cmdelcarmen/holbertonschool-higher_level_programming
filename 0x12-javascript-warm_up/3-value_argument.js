3eac9382769d07baa6ccfe5648bd8571b6b3c0ee
cmdelcarmen <delcarmencaroline@gmail.com>
2021-08-02 23:07:49 +0000

    Script prints first argument passed to itw

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+/*
+ * Script prints first argument passed to it.
+ */
+
+const args = process.argv;
+
+if (args.length <= 3) {
+  console.log(process.argv[2]);
+}
+if (args.length <= 2) {
+  console.log('No argument');
+}
