95b1fc5839987443ba74a457baa8ca4a47013d52
cmdelcarmen <delcarmencaroline@gmail.com>
2021-08-03 04:03:07 +0000

    Script adds two integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+/*
+ * Script that prints the addition of 2 integers
+ */
+
+const args = process.argv;
+let sum = 0;
+
+if (isNaN(args[2]) || isNaN(args[3])) {
+  console.log('NaN');
+} else {
+  sum = parseInt(args[2]) + parseInt(args[3]);
+  console.log(sum);
+}
