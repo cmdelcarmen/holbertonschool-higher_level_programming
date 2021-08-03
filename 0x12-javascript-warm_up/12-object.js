00fd9f36262ac17278d817732ed4a878f362c9ca
cmdelcarmen <delcarmencaroline@gmail.com>
2021-08-03 04:58:53 +0000

    Change value to 89 from 12

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+/*
+ * Script replces 12 with 89
+ */
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+myObject.value = 89;
+
+console.log(myObject);
