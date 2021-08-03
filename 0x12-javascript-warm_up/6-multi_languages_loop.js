e8bfe4f24b2affbd4d90fb31e70cb39e8882b110
cmdelcarmen <delcarmencaroline@gmail.com>
2021-08-03 03:24:16 +0000

    Script prints 3 lines using an array

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+/*
+ * Scipt prints 3 lines using an array
+ */
+
+const array = ['C is fun', 'Python is Cool', 'JavaScript is amazing'];
+let idx = 0;
+for (idx = 0; idx < 3; idx++) {
+  console.log(array[idx]);
+}
