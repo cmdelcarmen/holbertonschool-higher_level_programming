e8707c03bad53df504b321979a1b39f4b37d53e8
cmdelcarmen <delcarmencaroline@gmail.com>
2021-08-03 03:14:56 +0000

    Scrip prints out a string only when it can be converted to a number

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+/*
+ * Script prints "My number: <1st arg converted to int>",
+ * if the 1st arg can be converted to an int.
+ */
+
+const args = process.argv;
+
+if (!(isNaN(args[2]))) {
+  console.log('My number: ' + parseInt(args[2]));
+} else {
+  console.log('Not a number');
+}
