707223520e1d79da08e66100cee148fda2594e91
cmdelcarmen <delcarmencaroline@gmail.com>
2021-08-03 03:38:34 +0000

    Script prints x times 'C is fun'

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+/*
+ * Script prints x times 'C is fun'
+ */
+
+const args = process.argv;
+let idx = 0;
+
+if (isNaN(args[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  for (idx = 0; idx < parseInt(args[2]); idx++) {
+    console.log('C is fun');
+  }
+}
