7b60c78339e540e582763a53e0c5bb01127f9655
cmdelcarmen <delcarmencaroline@gmail.com>
2021-08-03 03:46:53 +0000

    Script prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+/*
+ * Script prints a square
+ */
+
+const args = process.argv;
+let idx = 0;
+let idx2 = 0;
+
+if (isNaN(args[2])) {
+  console.log('Missing size');
+} else {
+  for (idx = 0; idx < parseInt(args[2]); idx++) {
+    for (idx2 = 0; idx2 < parseInt(args[2]); idx2++) {
+      process.stdout.write('x');
+    }
+    console.log();
+  }
+}
