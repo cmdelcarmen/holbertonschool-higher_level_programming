84805b862a677947013f44556264ff0648ca7fda
cmdelcarmen <delcarmencaroline@gmail.com>
2021-08-05 16:08:25 +0000

    Script computes and prints a factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+function factorial (n) {
+  if (n < 0 || n === 0) { return (1); } else {
+    return (n * factorial(n - 1));
+  }
+}
+
+const args = process.argv;
+
+if (isNaN(args[2])) {
+  console.log('1');
+} else {
+  console.log(factorial(args[2]));
+}
