e70eb55bab6afb3bdf3bcf8f134fff89482775e6
cmdelcarmen <delcarmencaroline@gmail.com>
2021-08-09 06:01:39 +0000

    Scripts list all cities in the database

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_city import City
+
+if __name__ == "__main__":
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+        Session = sessionmaker(engine)
+        session = Session()
+
+        for city, state in session.query(City, State).order_by(City.id):
+            print("{}: ({}) {}".format(state.name, city.id, city.name))
