bbe4460bf3348af7dad91e31d38c557d97da5a2b
cmdelcarmen <delcarmencaroline@gmail.com>
2021-09-04 20:05:04 +0000

    Write a function that finds a peak in a list of unsorted integers.

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,6 +2,7 @@
 """ Write a function that finds a peak in a
     list of unsorted integers. """
 
+
 def find_peak(list_of_integers):
     """ Method finds a peak in a list of
     unsorted integers. """
