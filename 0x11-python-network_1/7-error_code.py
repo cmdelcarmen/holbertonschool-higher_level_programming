17830b00c9725b8655d567e8134460ec018491e3
cmdelcarmen <delcarmencaroline@gmail.com>
2021-09-04 17:09:31 +0000

    Write a Python script that takes in a URL, sends a request to the URL and displays the body of the response.

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Write a Python script that fetches
+    https://intranet.hbtn.io/status
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+    request = requests.get(url)
+
+    if request.status_code > = 400:
+        print("Error code: ", request.status_code)
+    else:
+        print(request.text)
