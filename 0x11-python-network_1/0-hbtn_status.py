4e9a30747aa3dfdf07f8a6e2ae9c00bc61ea0e0b
cmdelcarmen <delcarmencaroline@gmail.com>
2021-09-04 04:31:58 +0000

    moved import

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """ Write a Python script that fetches https://intranet.hbtn.io/stat """
 
-if __name__ == "__main__":
+import urllib.request
 
-    import urllib.request
+if __name__ == "__main__":
 
     with urllib.request.urlopen("https://intranet.hbtn.io/status") as request:
         html = request.read()
         print("Body response:")
-        print("\t - type: {}".format(type(html)))
-        print("\t - content: {}".format(html))
-        print("\t - utf8 content: {}".format(html.decode("utf-8")))
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
