7e39b13bc1286da18b04d4c7c2dc50ea760f7403
Caroline Del Carmen <delcarmencaroline@gmail.com>
2021-09-14 23:40:33 +0000

    Write a script that display the status code of a GET request.

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+/*
+* Write a script that display the status code of a GET request.
+*/
+
+const process = require('process');
+const request = require('request');
+const URL = process.argv[2];
+
+request(URL, function (error, res, content) {
+  if (error) {
+    console.error(error);
+  }
+  console.log(res.statusCode);
+});
