4249bfe7b7b9d6ce1bfdfa973a8ea1c9db7ae764
Caroline Del Carmen <delcarmencaroline@gmail.com>
2021-09-15 03:59:28 +0000

    h

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+/*
+ * Write a script that prints all characters of a Star Wars movie:
+ */
+
+const request = require('request');
+const films = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(films, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+});
