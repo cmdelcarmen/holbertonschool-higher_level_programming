74a3cc1b5126c4823efa7106aad00151dff994e5
Caroline Del Carmen <delcarmencaroline@gmail.com>
2021-09-15 03:44:31 +0000

    Write a script that prints the number of movies where the character “Wedge Antilles” is present.

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,42 +1,28 @@
 #!/usr/bin/node
 
 /*
-* Write a script that prints the number of movies where
-* the character “Wedge Antilles” is present.
-*/
-
+ Write a script that prints the number of movies where the character “Wedge Antilles” is present.*
+ */
 
 const request = require('request');
-const s = require('util');
-const URL = 'https://swapi-api.hbtn.io/api/films/';
-let count = 0;
+const urlAPI = process.argv[2];
+const characterID = 18;
+const characterUrl = 'https://swapi-api.hbtn.io/api/people/' + characterID + '/';
 
-request(URL, function (error, res, content) {
+request(urlAPI, (error, res, content) => {
   if (error) {
     console.error(error);
+    return;
   }
-  data = JSON.parse(content).results
+  const data = JSON.parse(content).results;
 
-  // let count = 0;
-  for (let movie = 0; movie < data.length; movie++) {
-
-  let list = data[movie].characters;
-
-  for (let idx = 0; idx < list.length; idx++) {
-
-  request(list[idx], function (errorB, resB, contentB) {
-    if (errorB) {
-      console.error(erroB);
-    }
-    nameOfCharacter = JSON.parse(contentB).name
-
-    if (nameOfCharacter == "Wedge Antilles") {
-      count++;
-      console.log(count);
+  let numberOfMovies = 0;
+  for (const movie of data) {
+    for (const character of movie.characters) {
+      if (character === characterUrl) {
+        numberOfMovies++;
       }
-  console.log(movie);
-  });
-  // console.log(count);
+    }
   }
-  }
+  console.log(numberOfMovies);
 });
