56bd5abf31f7ee468e24c677a5e39e09eef9e4b3
Caroline Del Carmen <77739870+cmdelcarmen@users.noreply.github.com>
2021-09-21 23:59:06 -0400

    Write a function that computes a to the power of b and return the value.
    Write a function that computes a to the power of b and return the value.
    
    Prototype: def pow(a, b):
    Returns the value of a ^ b
    You are not allowed to import any module

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
-
+'''
+    Write a function that computes a to the power of b and return the value.
+'''
 for letter in range(90, 64, -1):
     if letter % 2 == 0:
         print("{}".format(chr(letter+32)), end="")
