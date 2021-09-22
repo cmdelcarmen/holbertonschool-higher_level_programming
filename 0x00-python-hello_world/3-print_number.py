221f52cfae03f524782e22fa15fd60c36e82547e
Caroline Del Carmen <77739870+cmdelcarmen@users.noreply.github.com>
2021-09-21 23:12:10 -0400

    Complete this source code.
    Complete this source code in order to print the integer stored in the variable number, followed by Battery street, followed by a new line.
    
    You can find the source code here
    The output of the script should be:
    the number, followed by Battery street,
    followed by a new line
    You are not allowed to cast the variable number into a string
    Your code must be 3 lines long
    You have to use the new print numbers tips (with .format(...))

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''
     Complete this source code in order to print the integer stored 
-    in the variable number, followed by Battery street, followed by a new line.
+    in the variable number, followed by Battery street & followed by a new line.
 '''
 number = 98
 print('{:d} Battery street'.format(number))
