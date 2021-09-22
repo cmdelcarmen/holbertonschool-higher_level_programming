36dd9842cdb7e6e95644b04ac0113ffe2b5bc067
Caroline Del Carmen <77739870+cmdelcarmen@users.noreply.github.com>
2021-09-21 23:54:18 -0400

    Write a program that prints the ASCII alphabet, in lowercase, not followed by a new line.
    Write a program that prints the ASCII alphabet, in lowercase, not followed by a new line.
    
    Print all the letters except q and e
    You can only use one print function with string format
    You can only use one loop in your code
    You are not allowed to store characters in a variable
    You are not allowed to import any module

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+'''
+    Write a program that prints the ASCII alphabet, in lowercase, not followed by a new line.
+'''
 for count in range(97, 123):
     if count != 101 and count != 113:
         print("{}".format(chr(count)), end="")
