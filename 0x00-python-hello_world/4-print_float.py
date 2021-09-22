df7f4da9e41f8f28663a7566e52a9a2a093c4e3d
Caroline Del Carmen <77739870+cmdelcarmen@users.noreply.github.com>
2021-09-21 23:13:02 -0400

    Complete the source code.
    Complete the source code in order to print the float stored in the variable number with a precision of 2 digits.
    
    You can find the source code here
    The output of the program should be:
    Float:, followed by the float with only 2 digits
    followed by a new line
    You are not allowed to cast number to string
    You have to use the new print formatting tips (with .format(...))

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+'''
+    Complete the source code in order to print the
+    float stored in the variable number with a precision of 2 digits.
+'''
 number = 3.14159
 print("Float: {:0.2f}".format(number))
