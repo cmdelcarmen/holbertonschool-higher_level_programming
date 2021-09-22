1103088ed1720e479f151a74dc1baadd5bbbbb8f
Caroline Del Carmen <77739870+cmdelcarmen@users.noreply.github.com>
2021-09-21 23:26:51 -0400

    Write a Python script that prints out a sentence.
    Write a Python script that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line.
    
    Use the function write from the sys module
    You are not allowed to use print
    Your script should print to stderr
    Your script should exit with the status code 1

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+''' Write a Python script that prints exactly and that
+    piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line.
+'''
 import sys
 
 sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
