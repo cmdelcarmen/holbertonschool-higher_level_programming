c4f956330bc8b0195952cd5d929e9affa3949a09
Caroline Del Carmen <77739870+cmdelcarmen@users.noreply.github.com>
2021-09-22 00:19:57 -0400

    Write a function that replaces an element in a list at a specific position.
    Write a function that replaces an element in a list at a specific position without modifying the original list (like in C).
    
    Prototype: def new_in_list(my_list, idx, element):
    If idx is negative, the function should return a copy of the original list
    If idx is out of range (> of number of element in my_list), the function should return a copy of the original list
    You are not allowed to import any module
    You are not allowed to use try/except

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-
-
+'''
+    Write a function that replaces an element in a list at a specific
+    position without modifying the original list (like in C).
+'''
 def new_in_list(my_list, idx, element):
 
     newCopy_my_list = my_list[:]
