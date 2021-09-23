13edd71f634322129f611f81c34bd3b9bc5d4d57
Caroline Del Carmen <77739870+cmdelcarmen@users.noreply.github.com>
2021-09-22 21:08:56 -0400

    Fixed code

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,12 +1,6 @@
-$(function () {
-  $.ajax({
-    crossDomain: true,
-    url: 'https://swapi.co/api/films/?format=json',
-    success: function (data) {
-      const listFilms = data.results;
-      $.each(listFilms, function (index, value) {
+$.get( "https://swapi-api.hbtn.io/api/films/?format=json", function( response ) {
+  const result = response.results
+  $.each(result, function (index, value) {
         $('UL#list_movies').append('<li>' + value.title + '</li>');
       });
-    }
-  });
 });
